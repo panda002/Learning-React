7b74e64621211289bc25e02608adfd4c6495c35f
[Sidharth Panda] <[sidharth0panda@gmail.com]>
2020-07-20 00:08:14 -0400

    Added Pages

---
+++ FirstWebSite/my-website/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HomePage = () => (
+    <>
+        <h1>Hello, Welcome to my first react page</h1>
+        <p>
+            I hope to learn new things from, the linkedin tutorial.
+        </p>
+    </>
+
+);
+
+export default HomePage;
