b5867c8761c99d6ea04e9c5b6ea8127f4171d213
panda002 <sidharth0panda@gmail.com>
2020-07-22 20:12:09 -0400

    Added the URL paraments for article list page

--- FirstWebSite/my-website/src/pages/ArticlesList.js
+++ FirstWebSite/my-website/src/pages/ArticlesList.js
@@ -15,7 +15,7 @@ const ArticlesList = () => (
                 <h3>{article.title}</h3>
                 <p>{article.content[0].substring(0, 150)}...</p>
             </Link>
-        ))};
+        ))}
     </>
 
 );
